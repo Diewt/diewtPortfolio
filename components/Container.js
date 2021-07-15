2019d67da45ecf6755d90504c7a263672aa994d3
Stone Sha <36431006+stonesha@users.noreply.github.com>
2021-07-15 11:24:14 -0700

    Added children props and starter meta tags

--- components/Container.js
+++ components/Container.js
@@ -25,9 +25,24 @@ export default function Container(props) {
 
     return(
         <div className='bg-white dark:bg-black'>
-            <head>
-            </head>
+            <Head>
+                <title>{meta.title}</title>
+                <meta name="robots" content="follow, index" />
+                <meta content={meta.description} name="description" />
+                <meta name="keywords" content="Chris Diewtragulchai, Chris, Diewtragulchai" />
+                <meta name="author" content="Chris Diewtragulchai" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta property="og:type" content={meta.type} />
+                <meta property="og:site_name" content="Chris Diewtragulchai" />
+                <meta property="og:description" content={meta.description} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:image" content={meta.image} />
+                {meta.date && (
+                  <meta property="article:published_time" content={meta.date} />
+                )}
+            </Head>
             <main>
+                {children}
                 <Footer />
             </main>
         </div>
